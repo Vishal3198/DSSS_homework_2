ebab6678db766e3738d4dffdd83b6814c4ab87ce
unknown <vishal31shendge@gmail.com>
2023-11-15 22:02:58 +0100

    updated files for pip installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='0.0.1',
+    description='A simple math quiz game',
+    url='https://github.com/<your-username>/math_quiz',
+    author='<your-name>',
+    author_email='<your-email>',
+    license='MIT',
+    packages=['math_quiz'],
+    install_requires=['random'],
+    tests_require=['unittest'],
+)
